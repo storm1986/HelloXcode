089cc1ac8e9bf23f384da6befe78992ba911aa55
Ler Zhi Yao <storm_1986@hotmail.com>
2019-05-02 22:16:15 +0800

    Initial Commit

---
+++ HelloXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by Ler Zhi Yao on 02/05/2019.
+//  Copyright © 2019 Ler Zhi Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

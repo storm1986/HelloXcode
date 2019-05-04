4db3894951e14ef7426cf03c4aefca24547a1112
Ler Zhi Yao <storm_1986@hotmail.com>
2019-05-05 00:10:19 +0800

    SAMS iOS8 - p54

--- HelloXcode/ViewController.swift
+++ HelloXcode/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var myMessage: UILabel
+        var myUnusedVariable: String
+        myMessage = UILabel(frame: CGRectMake(30.0,50.0,300.0,50.0,50.0))
+        myMessage.font = UIFont.systemFontSize(48.0)
+        myMessage.text = "Hello Xcode"
+        myMessage.textColor = UIColor(patternImage: UIImage(named: "Background")!)
+        view.addSubview(myMessage)
+        NSLog("Hello Xcode, Again")
+    }
+    
+    // MARK: - Method for handling memory management
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
